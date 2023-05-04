3caef082ddc6561ff01ab50cf2737d30e0d64415
NataliaLuz <128393063+NataliaLuz@users.noreply.github.com>
2023-05-03 17:47:14 -0800

    Se subio el sistema de notas

---
+++ pie.php
@@ -0,0 +1,8 @@
+<!--pie.php-->
+<footer class="px-2 py-2 fixed-bottom bg-dark text-center">
+<span class="text-white">By LUZ NATALIA PARADA <?php echo date("Y"); ?>
+</span>
+</footer>
+</main>
+</body>
+</html>
